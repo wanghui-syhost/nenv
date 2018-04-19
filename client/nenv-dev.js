197630bf5451fba6e2c1374c743331d84e8e16a6
wanghui <wanghui2@infore.com>
2018-04-19 15:50:02 +0800

    nenvdev

--- client/nenv-dev.js
+++ client/nenv-dev.js
@@ -1,49 +1,8 @@
 import initNenv, * as nenv from './'
 import initWebpackHMR from './webpack-hot-middleware-client'
-import './dev.scss'
-const devPane = {
-  name: "DEVPANE",
-  data () {
-    return {
-      open: false
-    }
-  },
-  methods: {
-    triggleOpen () {
-      const self = this
-      self.open = !self.open
-    }
-  },
-  render (h) {
-    const self = this
-    return h(
-      'div',
-      {
-        staticClass: 'dev-wrapper'
-      },
-      [
-        h(
-          'span',
-          {
 
-          },
-          [
-            '展开'
-          ]
-        ),
-        self.open ? h(
-          'div',
-          {
-            staticClass: 'dev-main'
-          },
-          [
-
-          ]
-        ) : null
-      ]
-    )
-  }
-}
+import devPane from './nenvDev'
+import router from '../lib/router'
 
 const doc = document
 initNenv()
@@ -52,7 +11,7 @@ initNenv()
       const el = doc.createElement('div')
       doc.body.appendChild(el)
       const DevPane = Vue.extend(devPane)
-      new DevPane().$mount(el)
+      new DevPane({ router }).$mount(el)
     })
     .catch((err) => {
       console.error(`${err.message}\n${err.stack}`)
