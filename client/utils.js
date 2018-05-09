55f1403dfee18e412f3a70666eab67b23c345c32
wanghui <wanghui2@infore.com>
2018-05-09 13:59:50 +0800

    修复时间 格式化空值报错

--- client/utils.js
+++ client/utils.js
@@ -28,7 +28,7 @@ export const browser = {
 }
 
 export function parseTime (time, cFormat) {
-  if (arguments.length === 0) {
+  if (arguments.length === 0 || !time) {
     return null
   }
   const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}'
